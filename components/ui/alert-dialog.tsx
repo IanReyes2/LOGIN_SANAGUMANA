4ec90cbb53747b035cc5f06272c3a79648c6ed7b
IanReyes2 <ian.vincent.reyes@adamson.edu.ph>
2025-09-09 10:00:06 +0800

    astigin

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
 function AlertDialog({
-  ...props
+  ...props 
 }: React.ComponentProps<typeof AlertDialogPrimitive.Root>) {
   return <AlertDialogPrimitive.Root data-slot="alert-dialog" {...props} />
 }
